7ab48ca5d62f802a039ebaccc7e66d989462e15e
schel-d <schel.d.dev@gmail.com>
2022-04-15 14:12:39 +1000

    prepare pasting function

---
+++ ts/temp-paste-data.ts
@@ -0,0 +1,33 @@
+export const PASTE_TEXT = "Pakenham 6:33am - - - 7:01am - 7:14am - - -\n" +
+  "Cardinia Road 6:37am 6:46am 6:53am - 7:05am 7:11am 7:18am - 7:26am -\n" +
+  "Officer 6:40am 6:49am 6:56am - 7:08am 7:14am 7:21am - 7:29am -\n" +
+  "Beaconsfield 6:44am 6:53am 7:00am - 7:12am 7:18am 7:25am - 7:33am -\n" +
+  "Berwick 6:47am 6:56am 7:03am - 7:15am 7:21am 7:28am - 7:36am -\n" +
+  "Narre Warren 6:50am 6:59am 7:06am - 7:18am 7:24am 7:31am - 7:39am -\n" +
+  "Hallam 6:54am 7:03am 7:10am - 7:22am 7:28am 7:35am - 7:43am -\n" +
+  "Dandenong 7:00am 7:09am 7:16am 7:22am 7:28am 7:34am 7:41am 7:45am 7:49am -\n" +
+  "Yarraman 7:25am 7:48am -\n" +
+  "Noble Park 7:06am 7:15am 7:22am 7:28am 7:34am 7:40am 7:47am 7:51am 7:55am -\n" +
+  "Sandown Park 7:08am 7:17am 7:24am 7:30am 7:36am 7:42am 7:49am 7:53am 7:57am -\n" +
+  "Springvale 7:10am 7:19am 7:26am 7:32am 7:38am 7:44am 7:51am 7:55am 7:59am -\n" +
+  "Westall 7:35am 7:58am 8:11am\n" +
+  "Clayton 7:15am 7:24am 7:31am 7:37am 7:43am 7:49am 7:56am 8:00am 8:04am 8:13am\n" +
+  "Departures 6am - 9am Pakenham line - To City, Friday Timetable\n" +
+  "Huntingdale 7:18am 7:27am 7:34am 7:40am 7:46am 7:52am 7:59am 8:03am 8:07am 8:16am\n" +
+  "Oakleigh 7:21am 7:30am 7:37am 7:43am 7:49am 7:55am 8:02am 8:06am 8:10am 8:19am\n" +
+  "Hughesdale 7:23am 7:32am 7:39am 7:45am 7:51am 7:57am 8:04am 8:08am 8:12am 8:21am\n" +
+  "Murrumbeena 7:25am 7:34am 7:41am 7:47am 7:53am 7:59am 8:06am 8:10am 8:14am 8:23am\n" +
+  "Carnegie 7:27am 7:36am 7:43am 7:49am 7:55am 8:01am 8:08am 8:12am 8:16am 8:25am\n" +
+  "Caulfield 7:31am 7:40am 7:47am 7:53am 7:59am 8:05am 8:12am 8:16am 8:20am 8:29am\n" +
+  "Malvern\n" +
+  "Armadale\n" +
+  "Toorak\n" +
+  "Hawksburn\n" +
+  "South Yarra 7:39am 7:48am 7:55am 8:01am 8:07am 8:13am 8:20am 8:24am 8:28am 8:37am\n" +
+  "Richmond 7:42am 7:51am 7:58am 8:04am 8:10am 8:16am 8:23am 8:27am 8:31am 8:40am\n" +
+  "Please note the order of the stops below this point can change\n" +
+  "Parliament 7:46am 7:55am 8:02am 8:08am 8:14am 8:20am 8:27am 8:31am 8:35am 8:44am\n" +
+  "Melbourne Central 7:48am 7:57am 8:04am 8:10am 8:16am 8:22am 8:29am 8:33am 8:37am 8:46am\n" +
+  "Flagstaff 7:50am 7:59am 8:06am 8:12am 8:18am 8:24am 8:31am 8:35am 8:39am 8:48am\n" +
+  "Southern Cross 7:52am 8:01am 8:08am 8:14am 8:20am 8:26am 8:33am 8:37am 8:41am 8:50am\n" +
+  "Flinders Street 7:56am 8:05am 8:12am 8:18am 8:24am 8:30am 8:37am 8:41am 8:45am 8:54am";
